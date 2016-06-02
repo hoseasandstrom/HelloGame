2dbee726eabb91a8f5219251a32ad3475335c97f
hoseasandstrom <Lakyn4ever>
2016-06-02 11:01:06 -0400

    Hello Game

---
+++ core/src/com/studenttheironyard/MyGdxGame.java
@@ -0,0 +1,64 @@
+package com.studenttheironyard;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	float x, y, xv, yv;
+
+	static final float MAX_VELOCITY = 100;
+	static final float DECELERATION = 0.99f;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		move();
+
+		Gdx.gl.glClearColor(1, 0, 0, 1); //wiping the frame
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //wiping and coloring the frame
+		batch.begin();
+		batch.draw(img, x, y); //drawing the img
+		batch.end();
+	}
+	public void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			xv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			xv = MAX_VELOCITY;
+		}//x++; //increases by 1 //y++; //increases by 1
+
+		float delta = Gdx.graphics.getDeltaTime();
+		y += yv * delta;
+		x += xv * delta;
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+
+	}
+	public float decelerate(float velocity) {
+		velocity = DECELERATION;
+		if(Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+}
+
